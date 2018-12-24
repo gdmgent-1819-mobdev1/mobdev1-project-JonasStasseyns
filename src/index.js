39daec519afabfe561072b76d8fcb725fcf473a0
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2018-12-24 13:11:04 +0100

    Feedback push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import Navigo from 'navigo';
 import handlebars, { compile } from 'handlebars';
 import './styles/main.sass';
+
 import routes from './routes';
 
 // Partials
 const header = require('./partials/header.handlebars');
 const footer = require('./partials/footer.handlebars');
-const test = require('./partials/form.handlebars');
+const menu = require('./partials/menu.handlebars');
 
 // Register the partial components
 handlebars.registerPartial('header', compile(header)({ title: 'Just another web app' }));
 handlebars.registerPartial('footer', compile(footer)({ text: 'Template made with love by GDM Ghent' }));
-handlebars.registerPartial('form', compile(test));
+handlebars.registerPartial('menu', compile(menu));
 
 // Router logic to load the correct template when needed
 const router = new Navigo(window.location.origin, true);
@@ -19,6 +20,7 @@ const router = new Navigo(window.location.origin, true);
 routes.forEach((route) => {
   router.on(route.path, () => {
     route.view();
+    router.updatePageLinks();
   });
 });
 
@@ -28,8 +30,5 @@ router.notFound(() => {
 });
 router.resolve();
 window.onload = () => {
-  document.onclick = (e) => {
-    e.preventDefault();
-    router.navigate(e.target.getAttribute('href'));
-  };
+  router.navigate(window.location.hash.split('/')[1]);
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-use-before-define */
 // Only import the compile function from handlebars instead of the entire library
 import { compile } from 'handlebars';
+import handlebars from 'handlebars';
 import update from '../helpers/update';
 import { getInstance } from '../firebase/firebase';
 
 // Import the template to use
 const homeTemplate = require('../templates/home.handlebars');
+const form = require('../partials/form.handlebars');
+const kotListTemplate = require('../partials/kotlistteplate.handlebars');
+
+
 
 export default () => {
   // Data to be passed to the template
@@ -13,7 +18,11 @@ export default () => {
   // Return the compiled template to the router
   update(compile(homeTemplate)({ user }));
 
+  // wait for compilation
+  const fileButton = document.querySelector('.filetest');
+  // fileButton.addEventListener('change', uploadFile);
   addEventlisteners();
+  firebaseRead();
 };
 
 const instance = getInstance();
@@ -21,10 +30,10 @@ const instance = getInstance();
 function checkLoggedIn() {
   instance.auth().onAuthStateChanged((user) => {
     if (user) {
-      console.log('Logged In');
+      // document.querySelector('.user-email').innerHTML = user.email;
     } else {
       console.log('Not logged In');
-      document.querySelector('.signin-form').style.opacity = 1;
+      document.querySelector('.form-signin').style.opacity = 1;
       // TODO replace css display with handlebars show partial thingy
     }
   });
@@ -34,11 +43,14 @@ checkLoggedIn();
 
 function SignIn(email, password) {
   instance.auth().signInWithEmailAndPassword(email, password).catch((error) => {
-    const errorMessage = error.message;
+    localNotification(error.message);
     // TODO create element for errormessage
     // TODO redirect
   });
 }
+
+handlebars.registerPartial('form', compile(form));
+
 
 function SignUp(email, password) {
   instance.auth().createUserWithEmailAndPassword(email, password).catch((error) => {
@@ -48,23 +60,28 @@ function SignUp(email, password) {
   });
 }
 
-function addEventlisteners(){
-  // document.querySelector('.signin-submit').addEventListener('click', () => {
-  //   const email = document.querySelector('.signin-email').value;
-  //   const password = document.querySelector('.signin-password').value;
-  //   SignIn(email, password);
 
-    // TMP add kot
-    document.querySelector('.add-kot-submit').addEventListener('click', () => {
-      const kotTitle = document.querySelector('.add-kot-title');
-      const kotDescription = document.querySelector('.add-kot-description');
-      const kotAddress = document.querySelector('.add-kot-address');
-      console.log('fire');
-      instance.database().ref('kots/').push({
-        title: kotTitle,
-        description: kotDescription,
-        address: kotAddress,
-      });
+function addEventlisteners() {
+  document.querySelector('.signin-submit').addEventListener('click', () => {
+    const email = document.querySelector('.signin-email').value;
+    const password = document.querySelector('.signin-password').value;
+    SignIn(email, password);
+  });
+
+  // document.querySelector('body').addEventListener('click', () => {
+  //   localNotification('Welcome to the crew, status-bar!');
+  // });
+
+  // Test
+  // TMP add kot
+  document.querySelector('.add-kot-submit').addEventListener('click', () => {
+    const kotTitle = document.querySelector('.add-kot-title').value;
+    const kotDescription = document.querySelector('.add-kot-description').value;
+    const kotAddress = document.querySelector('.add-kot-address').value;
+    instance.database().ref('kots').push({
+      title: kotTitle,
+      description: kotDescription,
+      address: kotAddress,
     });
   });
 
@@ -73,4 +90,35 @@ function addEventlisteners(){
   //   const password = document.querySelector('.signup-password').value;
   //   SignUp(email, password);
   // });
+}
+
+function uploadFile(e) {
+  const file = e.target.files[0];
+  const storageRef = instance.storage().ref('testmap/' + file.name);
+  storageRef.put(file);
+}
+
+function firebaseRead() {
+  // document.querySelector('.kotlist').innerHTML = '';
+  const leadsRef = instance.database().ref();
+  leadsRef.on('value', (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const data = childSnapshot.val();
+      console.log(data.title);
+    });
+    console.log(snapshot.val());
+    const template = handlebars.compile(kotListTemplate);
+    const templateData = template(snapshot.val());
+    document.querySelector('.kotlist').innerHTML = templateData;
+    console.log(templateData);
+  });
+}
+
+function localNotification(message) {
+  const status = document.querySelector('.status-bar');
+  status.innerHTML = message;
+  status.style.display = 'block';
+  setTimeout(() => {
+    status.style.display = 'none';
+  }, 5000);
 }
