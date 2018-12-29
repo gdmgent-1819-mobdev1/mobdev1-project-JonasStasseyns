5afe1f88a37755e562baf967f5ea8e22d882c1b0
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2018-12-29 22:12:45 +0100

    New message system almost done

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -23,6 +23,37 @@ export default () => {
 };
 
 function getMessages() {
+  // Eventlistener for new message
+  document.querySelector('.new-message').addEventListener('click', () => {
+    document.querySelector('.message-list').innerHTML = '';
+    document.querySelector('.message-list').innerHTML = compile(composePartial)({
+      newmessage: true,
+    });
+    document.querySelector('.towhom').addEventListener('input', () => {
+      document.querySelector('.foundrecipcontainer').innerHTML = '';
+      if (document.querySelector('.towhom').value !== '') {
+        const recipQuery = firebase.database().ref('users').orderByChild('first');
+        recipQuery.on('value', (snapshot) => {
+          snapshot.forEach((result) => {
+            const recip = result.val();
+            const recipName = `${recip.first} ${recip.last}`;
+            // console.log(result.val());
+            if (recipName.toLowerCase().includes(document.querySelector('.towhom').value.toLowerCase())) {
+              document.querySelector('.foundrecipcontainer').innerHTML += '<p class="found-recip" id="' + result.key + '">' + recipName + '</p>';
+            }
+          });
+          const foundRecipElements = document.querySelectorAll('.found-recip');
+          foundRecipElements.forEach((recipElement) => {
+            recipElement.addEventListener('click', (e) => {
+              // TODO Display selected recipient
+              // TODO Make sure this is where the message is sent
+            });
+          });
+        });
+      }
+    });
+  });
+  // Actual messages fetching
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       const users = firebase.database().ref('users');
@@ -32,11 +63,11 @@ function getMessages() {
           currentUser = child.val();
           const nameKey = `${currentUser.first}_${currentUser.last}`;
           console.log(nameKey);
-          const qry = firebase.database().ref('messages/' + nameKey).orderByKey();
+          const qry = firebase.database().ref(`messages/${nameKey}`).orderByKey();
           qry.on('value', (snapshot) => {
             document.querySelector('.message-list').innerHTML = '';
             snapshot.forEach((message) => {
-              console.log('Raw: ' + message.val());
+              console.log(`Raw: ${message.val()}`);
               const compiledMessages = compile(messagePartial)({
                 fromto: message.key,
                 message: message.val(),
@@ -49,9 +80,9 @@ function getMessages() {
                   console.log('messageClick');
                   if (document.querySelector('.message-list')) {
                     document.querySelector('.message-list').innerHTML = '';
-                    const composeQuery = firebase.database().ref('messages/' + nameKey + '/' + fromName);
+                    const composeQuery = firebase.database().ref(`messages/${nameKey}/${fromName}`);
                     composeQuery.on('value', (snapshot) => {
-                      console.log('data: ' + fromName);
+                      console.log(`data: ${fromName}`);
                       let cleanStr = snapshot.val();
                       if (cleanStr.includes('You: ')) {
                         cleanStr = cleanStr.split('You: ').pop();
@@ -59,12 +90,14 @@ function getMessages() {
                           from: snapshot.key,
                           mess: cleanStr,
                           fromyou: true,
+                          newmessage: false,
                         });
                       } else {
                         document.querySelector('.message-list').innerHTML = compile(composePartial)({
                           from: snapshot.key,
                           mess: snapshot.val(),
                           fromyou: false,
+                          newmessage: false,
                         });
                       }
                       document.querySelector('.send-message').addEventListener('click', (e) => {
@@ -72,11 +105,11 @@ function getMessages() {
                         const sendContent = document.querySelector('.compose-textarea').value;
                         console.log(to);
                         console.log(sendContent);
-                        firebase.database().ref('messages/' + to).set({
+                        firebase.database().ref(`messages/${to}`).set({
                           [nameKey]: sendContent,
                         });
-                        firebase.database().ref('messages/' + nameKey).set({
-                          [to]: 'You: ' + sendContent,
+                        firebase.database().ref(`messages/${nameKey}`).set({
+                          [to]: `You: ${sendContent}`,
                         });
                       });
                     });
